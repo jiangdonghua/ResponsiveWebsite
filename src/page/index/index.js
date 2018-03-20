e5538bd751795c08a06a0c543a657d21fb3892d1
jiangdonghua <861252878@q.com>
2018-03-20 11:01:57 +0800

    解决首页input聚焦

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -7,15 +7,20 @@ require('util/lib/modal.js')
 var wowAnimate=require('util/lib/wow.js')
 console.log('i am index')
 
-
 new wowAnimate.WOW({
     offset: 0
 }).init();
+
 //当调用 slide 实例方法时立即触发该事件。
 $("#home .carousel").on("slide.bs.carousel",function () {
     var _this=$(this);
     _this.find(".carousel-inner .item .wow:not(.animated)").addClass("animated")
-})
+});
+//当调用 activate 实例方法时立即触发该事件。
+$('[data-spy="scroll"]').on("activate.bs.scrollspy", function() {
+    $(".navbar-boss").attr("data-nav", $(".navbar-boss .nav li.active a").attr("href"))
+});
+//兼容移动端
 $("section#home .carousel").swipe({
     swipe: function(n, t) {
         t.toString() == "right" && $(".carousel").carousel("prev");
@@ -38,18 +43,21 @@ $("body").swipe({
         direction.toString() == "down" && nSwitch(!0)
     }
 });
+//滚屏
 $('body').on('mousewheel', function(event) {
     nSwitch(event.deltaY > 0);
 });
+//调整浏览器窗口大小
 $(window).resize(function() {
     $(".navbar-boss .navbar-respond li.active a").click()
 });
+//判断滚屏下一页
 var nSwitch = function(status) {
     var _active = $(".navbar-boss .navbar-respond li.active"),
         willDom = status ? _active.prev() : _active.next();
     willDom && willDom.find("a").click()
 };
-
+//兼容移动
 $("section#abouts").swipe({
     swipe: function(event, direction) {
         var i = function(n) {
@@ -60,6 +68,4 @@ $("section#abouts").swipe({
         direction.toString() == "right" && i(-1)
     }
 })
-$('[data-spy="scroll"]').on("activate.bs.scrollspy", function() {
-    $(".navbar-boss").attr("data-nav", $(".navbar-boss .nav li.active a").attr("href"))
-});
+
